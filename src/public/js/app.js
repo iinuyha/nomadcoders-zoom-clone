d8ec660b7221ee0e3173c7487cab4e266877a4d0
Son Ahyun <dkgus731@naver.com>
2024-11-12 11:42:09 +0900

    카메라 세팅 + 채팅룸 생성

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,93 +1,118 @@
-const socket = io(); // 알아서 socket.io를 실행하고 있는 서버 찾기
+const socket = io();
+
+//////////////////// phone call
+
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
+
+const call = document.getElementById("call");
+
+let roomName;
+
+call.hidden = true;
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+async function getCameras() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput"); // return 추가
+    const currentCamera = myStream.getVideoTracks()[0];
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      camerasSelect.appendChild(option);
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function getMedia(deviceId) {
+  const initialConstraints = {
+    audio: true,
+    video: { facingMode: "user" },
+  };
+
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
+
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstraints
+    );
+    myFace.srcObject = myStream;
+    if (!deviceId) {
+      await getCameras(); // getMedia 함수에서 카메라 목록을 가져오도록 호출
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function handleMuteClick() {
+  myStream.getAudioTracks().forEach((track) => (track.enabled = muted));
+  if (!muted) {
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  } else {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+}
+
+function handleCameraClick() {
+  myStream.getVideoTracks().forEach((track) => (track.enabled = cameraOff));
+  if (!cameraOff) {
+    cameraBtn.innerText = "Turn Camera On";
+    cameraOff = true;
+  } else {
+    cameraBtn.innerText = "Turn Camera Off";
+    cameraOff = false;
+  }
+}
+
+function handleCameraChange() {
+  getMedia(camerasSelect.value);
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
+
+//////////////////// Welcome Form (join a room)
 
 const welcome = document.getElementById("welcome");
-const searchRoom = welcome.querySelector("#searchRoom");
-const nameForm = welcome.querySelector("#name");
-const room = document.getElementById("room");
+const welcomeForm = welcome.querySelector("form");
 
-room.hidden = true; // 초기엔 room div가 가려지기
+function startMedia() {
+  welcome.hidden = true;
+  call.hidden = false;
+  getMedia();
+}
 
-let roomName; // 채팅룸 이름을 담을 수 있는 변수 선언
-
-searchRoom.addEventListener("submit", handleRoomSubmit);
-nameForm.addEventListener("submit", handleNicknameSubmit);
-
-// 방 검색 버튼 클릭 시 실행
-function handleRoomSubmit(event) {
-  event.preventDefault(); // 기본 동작을 제거하고, 아래에서 설정한 커스텀 동작들을 실행하겠단 의미
-  const input = searchRoom.querySelector("input");
-  socket.emit("enter_room", input.value, showRoom); // 소켓에 "enter_room" 이벤트를 발생시키고, input의 값, showRoom함수를 전달
+function handleWelcomeSubmit(event) {
+  event.preventDefault();
+  const input = welcomeForm.querySelector("input");
+  socket.emit("join_room", input.value, startMedia);
   roomName = input.value;
   input.value = "";
 }
 
-// 닉네임 저장 버튼 클릭 시 실행
-function handleNicknameSubmit(event) {
-  event.preventDefault(); // 기본 동작을 제거하고, 아래에서 설정한 커스텀 동작들을 실행하겠단 의미
-  const input = welcome.querySelector("#name input");
-  socket.emit("nickname", input.value); // 소켓에 "nickname" 이벤트를 발생시키고, input의 값을 전달
-}
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 
-// room박스가 보여지는 함수
-function showRoom() {
-  welcome.hidden = true; // welcome div가 가려지기
-  room.hidden = false; // room div가 보여지기
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName}`;
-  const msgForm = room.querySelector("#msg");
-  msgForm.addEventListener("submit", handleMessageSubmit);
-}
+//////////////////// socket code
 
-// ul부분에 새로운 li를 추가하여 메시지를 보여주는 함수
-function addMessage(msg) {
-  const ul = room.querySelector("ul");
-  const li = document.createElement("li");
-  li.innerText = msg;
-  ul.appendChild(li);
-}
-
-// 메시지 전송 버튼 클릭 시 실행
-function handleMessageSubmit(event) {
-  event.preventDefault(); // 기본 동작을 제거하고, 아래에서 설정한 커스텀 동작들을 실행하겠단 의미
-  const input = room.querySelector("#msg input");
-  const value = input.value;
-
-  // 소켓에 "new_message" 이벤트를 발생시키고, input의 값, 방 이름, 이후 실행될 함수 전달
-  socket.emit("new_message", input.value, roomName, () => {
-    addMessage(`You: ${value}`);
-  });
-  input.value = "";
-}
-
-// welcome 이벤트가 실행이 되면 addMessage 함수를 실행
-socket.on("welcome", (user, newCount) => {
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName} (${newCount})`;
-  addMessage(`${user}님이 참여했습니다!`);
-});
-
-// bye 이벤트가 실행이 되면 addMessage 함수를 실행
-socket.on("bye", (user, newCount) => {
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName} (${newCount})`;
-  addMessage(`${user}님이 퇴장했습니다.`);
-});
-
-// new_message 이벤트가 실행이 되면 addMessage 함수 실행
-socket.on("new_message", (msg) => {
-  addMessage(msg);
-});
-
-// room_change 이벤트가 실행이 되면 채팅룸 목록에 추가
-socket.on("room_change", (rooms) => {
-  const roomList = welcome.querySelector("ul");
-  roomList.innerHTML = "";
-  if (rooms.length === 0) {
-    return;
-  }
-  rooms.forEach((room) => {
-    const li = document.createElement("li");
-    li.innerText = room;
-    roomList.append(li);
-  });
+socket.on("welcome", () => {
+  console.log("someone joined");
 });
--- src/server.js
+++ src/server.js
@@ -13,69 +13,19 @@ app.use("/public", express.static(__dirname + "/public")); // static 설정 (기
 app.get("/", (req, res) => res.render("home")); // 루트경로에서 /views/home.pug를 렌더링
 app.get("/*", (req, res) => res.redirect("/")); // 사용자가 어떤 경로로 가던 루트경로로 리다이렉션
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-
 // http 프로토콜을 사용하는 서버 생성
 const httpServer = http.createServer(app); // "http://localhost:3000"에서 실행
 // http서버 위에 웹소켓 프로토콜을 사용하는 서버 생성
 const io = SocketIO(httpServer);
 
-function publicRooms() {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = io;
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-  return publicRooms;
-}
-
-// 클라이언트가 서버에 연결되면 socket 객체를 통해 클라이언트와의 연결이 생성
 io.on("connection", (socket) => {
-  socket["nickname"] = "Anon"; // 소켓의 기본 nickname속성의 값을 Anon(익명)으로 설정
-
-  // 클라이언트에서 발생하는 모든 이벤트를 가로채서 이벤트 이름을 콘솔에 출력
-  socket.onAny((event) => {
-    console.log(`Socket Event: ${event}`);
-  });
-
-  function countRoom(roomName) {
-    return io.sockets.adapter.rooms.get(roomName)?.size;
-  }
-
-  // "enter_room"이벤트가 발생되면
-  socket.on("enter_room", (roomName, done) => {
-    socket.join(roomName); // roomName이라는 이름의 방에 현재 소켓(클라이언트)을 참여시킴
+  socket.on("join_room", (roomName, done) => {
+    socket.join(roomName);
     done();
-    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName)); // 특정 방(roomName)에 있는 클라이언트들(자신 제외)에게만 welcome 이벤트를 전송
-    io.sockets.emit("room_change", publicRooms()); // "room_change" 이벤트를 모두에게 발생시킴
+    socket.to(roomName).emit("welcome");
   });
-
-  // 소켓의 연결이 해제되면
-  socket.on("disconnecting", () => {
-    // 각각의 채팅룸에 대해서 bye 이벤트를 발생시키기
-    socket.rooms.forEach((room) => {
-      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1);
-    });
-  }); // 자신이 아닌 모든 브라우저에 대해서 bye 이벤트를 발생시키기
-
-  socket.on("disconnect", () => {
-    io.sockets.emit("room_change", publicRooms()); // "room_change" 이벤트를 모두에게 발생시킴
-  });
-
-  // "new_message"이벤트가 발생되면
-  socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-    done();
-  });
-
-  // 닉네임 저장 이벤트가 발생하면 socket의 닉네임 속성을 입력받은 nickname으로 설정하기
-  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 });
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 httpServer.listen(3000, handleListen);
